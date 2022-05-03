2d6b42831fd831b08bcef6b0b3af7e39612d848b
anasaxen <anand.saxena@publicissapient.com>
2022-05-03 12:21:07 +0530

    update for creatre Document

--- client/dom.js
+++ client/dom.js
@@ -3,7 +3,7 @@ function createDomElement(element, attributes) {
     let newElement = document.createElement(element);
     if(attributes){
         Object.keys(attributes).forEach(key => {
-            newElement.setAttribute(key, attributes[key]);
+            newElement.setAttribute (key, attributes[key]);
         });
     }
     return newElement;
